ff954efe477fcbb6f73ea9dd34fcc1afb63c2db2
Dana AlSiddig <danlsiddig@gmail.com>
2023-10-09 18:50:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Please read the README.md to understand what to do. Happy Coding !
+const fetchUserData = async () => {
+    try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
+    const userData = await response.json();
+
+    const modifiedUserData = userData.map((user) => {
+        return {
+            Name: user.name,
+            Username: user.username,
+            Email: user.email,
+            Address: user.address.street,
+            Phone: user.phone,
+            Website: user.website,
+            Company: user.company.name,
+        }
+    })
+    console.log(modifiedUserData);
+
+    const filteredUserData = modifiedUserData.filter((user) => {
+        return user.email.endsWith('.biz')
+    })
+    console.log(filteredUserData);
+
+    const sortedUserData = filteredUserData.sort((a, b) => a.firstname.localeCompare(b.firstname))
+    console.log(sortedUserData);
+    
+} catch (error) {
+    console.error('Error:', error);
+}
+}
